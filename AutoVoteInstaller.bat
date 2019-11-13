4b7253977870d61c7ca94ec9b916857420c4d98e
Sean McGuire <16692089+SPMNJ@users.noreply.github.com>
2019-11-13 16:49:42 -0500

    Update AutoVoteInstaller.bat

--- AutoVoteInstaller.bat
+++ AutoVoteInstaller.bat
@@ -3,7 +3,8 @@ set lol=%USERNAME%
 @echo Installing for user: %lol%
 bitsadmin /cancel votescript-download
 bitsadmin /cancel requests-download
-msiexec /i C:\Users\%lol%\Downloads\python-2.7.15.msi TARGETDIR=C:\Users\%lol%\Downloads\Python /qb!
+bitsadmin /transfer temp-requests https://www.python.org/ftp/python/2.7.16/python-2.7.16.msi C:\Users\%lol%\Downloads\python2.7.16.py
+msiexec /i C:\Users\%lol%\Downloads\python-2.7.16.msi TARGETDIR=C:\Users\%lol%\Downloads\Python /qb!
 bitsadmin /transfer votescript-download https://github.com/SPMNJ/Voting-Bot/raw/master/install.py C:\Users\%lol%\Downloads\install.py
 bitsadmin /transfer requests-download https://github.com/SPMNJ/Voting-Bot/raw/master/requests.bat %localappdata%\Temp\requests.bat
 @echo Loading Installer
